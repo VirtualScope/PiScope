95059c6988af89b7f7f65d37d5fa45c979893c03
reptilicus <xz7681dh@metrostate.edu>
2020-04-25 13:45:56 -0500

    Finished config settings and cleaned up

--- Raspberry/scope.py
+++ Raspberry/scope.py
@@ -21,11 +21,13 @@ except:
 
 #Original pi code.
 class scope(): 
-  def __init__(self, my_name):
+  def __init__(self):
     #Define the microscope name !!IMPORTANT it comes from terminal argument
     #my_name = sys.argv[1]
 
     config = configparser.ConfigParser()
+
+    #If the config file doesn't exist, prompt the user for config info
     if len(config.read('virtualscope.ini')) == 0:
       config['Database'] = {'Host IP': input("Enter the host IP Address: "),
                             'Database Name': input("Enter the database name: "),
@@ -37,25 +39,32 @@ class scope():
                             'Username': input("Enter the FTP username: "),
                             'Password': input("Enter the FTP password: ")}
 
+      config['Miscellaneous'] = {'Microscope Name': input("Enter the name of this microscope: ")}
       config['Miscellaneous'] = {'Pictures Path': '/home/pi/MicroscopeImages/'}
+      config['Miscellaneous'] = {'Working Images Path': 'public_html/microscopes/' + config['Miscellaneous']['Microscope Name'] + '/images/'}
+
       with open('virtualscope.ini', 'w') as configfile:
         config.write(configfile)
 
     config.read('virtualscope.ini')
-    #Establish the database connection
-    database = config['Database']
 
+    #Grab settings from config file
+    databaseConfig = config['Database']
+    ftpConfig = config['FTP']
+    miscConfig = config['Miscellaneous']
+
+    #Establish the database connection
     try:
-      connection = mysql.connector.connect(host=database['Host IP'],
-                          database=database['Database Name'],
-                          user=database['Username'],
-                          password=database['Password'])
+      connection = mysql.connector.connect(host=databaseConfig['Host IP'],
+                          database=databaseConfig['Database Name'],
+                          user=databaseConfig['Username'],
+                          password=databaseConfig['Password'])
 
       if connection.is_connected():
         #Select the time increment from the microscopes table
         cursor = connection.cursor()
         select_stmt = "SELECT picture_time_increment, youtube_stream FROM microscopes WHERE microscope_name = %(microscope_name)s"
-        cursor.execute(select_stmt, { 'microscope_name': my_name })
+        cursor.execute(select_stmt, { 'microscope_name': miscConfig['Microscope Name'] })
         info = cursor.fetchone()
         time_increment = info[0]
         stream_link = info[1]
@@ -64,6 +73,8 @@ class scope():
     except Error as e:
       print("Error while connecting to MySQL", e)
       time_increment = 3
+      if (not('connection' in locals())):
+          sys.exit("Error while connecting to MySQL. Check virtualscope.ini Exiting...")
       
     #Close the database connection
     finally:
@@ -73,20 +84,20 @@ class scope():
 
     #Connect to FTP server for file uploading
     ftp = ftplib.FTP()
-    host = "ftp.virtualscope.site"
-    port = 21
+    host = ftpConfig['Hostname']
+    port = int(ftpConfig['Port'])
     ftp.connect(host, port)
-    ftp.login("teampuma","1#%ekd%YlaG*")
-    ftp.cwd("public_html/microscopes/" + my_name + "/images/")
+    ftp.login(ftpConfig['Username'], ftpConfig['Password'])
+    ftp.cwd(miscConfig['Working Images Path'])
 
     #The concatonated command for streaming
     stream_command = "raspivid -o - -t 0 -w 1280 -h 720 -fps 30 -b 6000000 | ffmpeg -re -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -g 50 -strict experimental -f flv " + stream_link
 
     #Picture folder where photos are saved on the Pi
-    pic_folder = "/home/pi/MicroscopeImages/"
+    pic_folder = miscConfig['Pictures Path']
 
     #NEW: Start Control Server
-    DeviceCommThread = threading.Thread(target=DeviceComm.start, args=(DeviceComm), name='DeviceCommMainThread')
+    DeviceCommThread = threading.Thread(target=DeviceComm.start, args=(DeviceComm,), name='DeviceCommMainThread')
     DeviceCommThread.start()
 
     while True:
@@ -110,9 +121,4 @@ class scope():
       ftp.storbinary("STOR " + picture_name, file)     # send the file
       file.close()
 
-scope_name = sys.argv[1]
-scope = scope(scope_name)
-
-#NEW: Start Control Server
-DeviceCommThread = threading.Thread(target=DeviceComm.start, args=(DeviceComm,), name='DeviceCommMainThread')
-DeviceCommThread.start()
+scope()
