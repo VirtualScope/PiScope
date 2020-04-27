03a9739e2e7a20bb61daedf8492d2a9fd488e78e
Reptilicus <xz7681dh@metrostate.edu>
2020-04-27 13:22:29 -0500

    Updated dependencies

--- Raspberry/startup.sh
+++ Raspberry/startup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-#Install necessary package dependencies
-apt install python
+#Check if script is being run as root
+if [ `whoami` != root ]; then
+    echo "To run this script, use \"sudo ./startup.sh\""
+    exit
+fi
 
-#Install python package dependencies
-pip install mysql-connector-python
+#If python3 is not installed, install Python3
+if ! dpkg -s python3 >/dev/null 2>&1; then
+    echo "Installing python3..."
+    apt install python3 >> startup.log
+fi
+
+#Install python package dependencies as user pi
+echo "Checking for python packages..."
+sudo -u pi pip3 install mysql-connector-python >> startup.log
+sudo -u pi pip3 install gevent >> startup.log
+sudo -u pi pip3 install flask_restful >> startup.log
+sudo -u pi pip3 install flask >> startup.log
 
 #Run server
-python scope.py
+echo "Starting VirtualScope..."
+python3 virtualscope.py
